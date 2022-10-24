66f1f2059c0c30fc9074a99a4706c9e79d07e83d
Jewoo <jeus0630@gmail.com>
2022-10-24 23:40:21 +0900

    Initial commit

---
+++ src/main/java/com/example/mungmatebackend/MungmateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.mungmatebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MungmateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MungmateBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mungmatebackend/MungmateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mungmatebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MungmateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
